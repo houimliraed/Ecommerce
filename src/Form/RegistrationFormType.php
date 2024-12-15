8b584f84056258ed0571937449d7336c64e715a4
rayxtn <houimliraed@outlook.fr>
2024-12-15 22:13:34 +0100

    added firstname and lastname

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,6 +26,8 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('firstName')
+            ->add('lastName')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
