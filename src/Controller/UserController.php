88dd3331008b5a7c6da4171083bff46b3016fe18
rayxtn <houimliraed@outlook.fr>
2024-12-19 03:41:26 +0100

    users roles and navbar fixed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +17,30 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
+    }
+
+
+    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')]
+    public function updateUserRole(EntityManagerInterface $entityManager, User $user): Response
+    {
+      $user->setRoles(["ROLE_EDITOR","ROLE_USER"]);
+      $entityManager->flush();
+      $this->addFlash('success','User updated role and added successfuly');
+      
+
+      return $this->redirectToRoute('app_user');
+    }
+
+
+    
+    #[Route('/admin/user/{id}/remove', name: 'app_user_remove')]
+    public function userRemove(EntityManagerInterface $entityManager,$id, UserRepository $userRepository): Response
+    {
+     $userfind = $userRepository->find($id);
+     $entityManager->remove($userfind);
+     $entityManager->flush();
+      
+     $this->addFlash('danger','User deleted !!');
+      return $this->redirectToRoute('app_user');
     }
 }
